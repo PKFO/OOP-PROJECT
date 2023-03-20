b662259879acadec6a04cbedc6a0f738f4dfcc80
perawat_kosit <perawat246@gmail.com>
2023-03-21 00:17:49 +0700

    add file

--- Class.py
+++ Class.py
@@ -1,16 +1,18 @@
-class dealer(Account):
-    def __init__(self, Dealer_Name, Office_hours, Area, Out_of_time, payment_info, insurerance, other, description, category, Review):
-        self.__Dealer_Name = Dealer_Name
-        self.__Office_hours = Office_hours
-        self.__Area = Area
-        self.__Out_of_time = Out_of_time
-        self.__payment_info = payment_info
-        self.__insurerance = insurerance
-        self.__other = other
-        self.__description = description
-        self.__category = category
-        self.__Review = Review
-
+from enum import Enum
+class dealer:
+    def __init__(self, Dealer_Name, Office_hours, Area, Out_of_time, payment_info, insurerance, other, description, category, Review, profile_image):
+        self._Dealer_Name = Dealer_Name
+        self._Office_hours = Office_hours
+        self._Area = Area
+        self._Out_of_time = Out_of_time
+        self._payment_info = payment_info
+        self._insurerance = insurerance
+        self._other = other
+        self._description = description
+        self._category = category
+        self._Review = Review
+        self._profile_image = profile_image
+    
     def Create_car():
         pass
 
@@ -34,33 +36,33 @@ class dealer(Account):
 
 class CarCategory:
     def __init__(self, car_with_driver, self_drive):
-        self.__car_with_driver = [car_with_driver]
-        self.__self_drive = [self_drive]
+        self.car_with_driver = [car_with_driver]
+        self.self_drive = [self_drive]
 
 class Car:
     def __init__(self, Car_Name, Equipment, Price, Car_image, Year, Gear, Type, Large_bag, Small_bag, passenger, Car_status):
-        self.__Car_Name= Car_Name
-        self.__Equipment = Equipment
-        self.__Price = Price
-        self.__Car_image = Car_image
-        self.__Year = Year
-        self.__Gear = Gear
-        self.__Type = Type
-        self.__Large_bag = Large_bag 
-        self.__Small_bag = Small_bag
-        self.__passenger = passenger
-        self.__Car_status = Car_status
-
+        self.Car_Name= Car_Name
+        self.Equipment = Equipment
+        self.Price = Price
+        self.Car_image = Car_image
+        self.Year = Year
+        self.Gear = Gear
+        self.Type = Type
+        self.Large_bag = Large_bag 
+        self.Small_bag = Small_bag
+        self.passenger = passenger
+        self.Car_status = Car_status
+        
     def check_car(self,car_status):
         pass
+
 class Account:
-    def __init__(self, username, name, surname, birthdate, email, tel):
-        self._username = username
-        self._name = name
-        self._surname = surname
-        self._birthdate = birthdate 
-        self._email = email
-        self._tel = tel  
+    def __init__(self, username, name, birth_date, profile_image, password):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.birthdate = birth_date 
+        self.profile_image = profile_image
 
     def check_info():
         pass
@@ -79,25 +81,23 @@ class Account:
     
     def get_birth_date(self):
         return self._birthdate
-    
-class Booking():
-    def __init__(self, First_Date, Last_Date, BookingStatus, price_added_on,Car_Name,amount):
-        self.__First_Date = First_Date 
-        self.__Last_Date = Last_Date
-        self.__BookingStatus = BookingStatus
-        self.__price_added_on = price_added_on
-        self.__Car_Name = Car_Name
-        self.__amount = amount
-        
+
+class Booking:
+    def __init__(self, First_Date, Last_Date, BookingStatus, price_added_on):
+        self.First_Date = First_Date 
+        self.Last_Date = Last_Date
+        self.BookingStatus = BookingStatus
+        self.price_added_on = price_added_on
 
     def update_bookingstatus(self,First_date,Last_date):
         pass
 
+
 class Region:
     def __init__(self, Region_TH, Province, Place):
-        self.__Region_TH = [Region_TH]
-        self.__Province = Province
-        self.__Place = Place
+        self.Region_TH = [Region_TH]
+        self.Province = Province
+        self.Place = Place
 
     def search_car():
         pass
@@ -112,26 +112,25 @@ class BookCar:
 
 class location:
     def __init__(self, get_car, return_car) -> None:
-        self.__get_car = get_car
-        self.__return_car = return_car
+        self.get_car = get_car
+        self.return_car = return_car
 
     def get_location(self):
         pass
 
 class promotion:
     def __init__(self, promotion_list, special_price):
-        self.__promotion_list = [promotion_list]
-        self.__special_price = special_price
+        self.promotion_list = [promotion_list]
+        self.special_price = special_price
 
     def get_promotion(self):
         pass
 
 class review:
     def __init__(self, rate, quality, service, accuracy, recommendation, comment):
-        self.__rate = rate
-        self.__quality = quality
-        self.__service = service
-        self.__accuracy = accuracy
-        self.__recommendation = recommendation
-        self.__comment = comment
-    
+        self.rate = rate
+        self.quality = quality
+        self.service = service
+        self.accuracy = accuracy
+        self.recommendation = recommendation
+        self.comment = comment
