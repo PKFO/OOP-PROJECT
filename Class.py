bece59ae07e8e2f3aa0f48a534b483418e5b0e0c
perawat_kosit <perawat246@gmail.com>
2023-03-21 00:21:54 +0700

    fix

--- Class.py
+++ Class.py
@@ -36,33 +36,33 @@ class dealer:
 
 class CarCategory:
     def __init__(self, car_with_driver, self_drive):
-        self.car_with_driver = [car_with_driver]
-        self.self_drive = [self_drive]
+        self.__car_with_driver = [car_with_driver]
+        self.__self_drive = [self_drive]
 
 class Car:
     def __init__(self, Car_Name, Equipment, Price, Car_image, Year, Gear, Type, Large_bag, Small_bag, passenger, Car_status):
-        self.Car_Name= Car_Name
-        self.Equipment = Equipment
-        self.Price = Price
-        self.Car_image = Car_image
-        self.Year = Year
-        self.Gear = Gear
-        self.Type = Type
-        self.Large_bag = Large_bag 
-        self.Small_bag = Small_bag
-        self.passenger = passenger
-        self.Car_status = Car_status
+        self._Car_Name= Car_Name
+        self._Equipment = Equipment
+        self._Price = Price
+        self._Car_image = Car_image
+        self._Year = Year
+        self._Gear = Gear
+        self._Type = Type
+        self._Large_bag = Large_bag 
+        self._Small_bag = Small_bag
+        self._passenger = passenger
+        self._Car_status = Car_status
         
     def check_car(self,car_status):
         pass
 
 class Account:
     def __init__(self, username, name, birth_date, profile_image, password):
-        self.username = username
-        self.password = password
-        self.name = name
-        self.birthdate = birth_date 
-        self.profile_image = profile_image
+        self.__username = username
+        self.__password = password
+        self.__name = name
+        self.__birthdate = birth_date 
+        self.__profile_image = profile_image
 
     def check_info():
         pass
@@ -84,10 +84,10 @@ class Account:
 
 class Booking:
     def __init__(self, First_Date, Last_Date, BookingStatus, price_added_on):
-        self.First_Date = First_Date 
-        self.Last_Date = Last_Date
-        self.BookingStatus = BookingStatus
-        self.price_added_on = price_added_on
+        self.__First_Date = First_Date 
+        self.__Last_Date = Last_Date
+        self.__BookingStatus = BookingStatus
+        self.__price_added_on = price_added_on
 
     def update_bookingstatus(self,First_date,Last_date):
         pass
@@ -95,9 +95,9 @@ class Booking:
 
 class Region:
     def __init__(self, Region_TH, Province, Place):
-        self.Region_TH = [Region_TH]
-        self.Province = Province
-        self.Place = Place
+        self.__Region_TH = [Region_TH]
+        self.__Province = Province
+        self.__Place = Place
 
     def search_car():
         pass
@@ -112,25 +112,25 @@ class BookCar:
 
 class location:
     def __init__(self, get_car, return_car) -> None:
-        self.get_car = get_car
-        self.return_car = return_car
+        self.__get_car = get_car
+        self.__return_car = return_car
 
     def get_location(self):
         pass
 
 class promotion:
     def __init__(self, promotion_list, special_price):
-        self.promotion_list = [promotion_list]
-        self.special_price = special_price
+        self.__promotion_list = [promotion_list]
+        self.__special_price = special_price
 
     def get_promotion(self):
         pass
 
 class review:
     def __init__(self, rate, quality, service, accuracy, recommendation, comment):
-        self.rate = rate
-        self.quality = quality
-        self.service = service
-        self.accuracy = accuracy
-        self.recommendation = recommendation
-        self.comment = comment
+        self.__rate = rate
+        self.__quality = quality
+        self.__service = service
+        self.__accuracy = accuracy
+        self.__recommendation = recommendation
+        self.__comment = comment
